6677f80f6671d9a39602372136b82d68486c656a
joebobbio <profjstark2@gmail.com>
2022-05-08 22:29:55 -0400

    hotfix

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,14 @@
 function myFunction(){
     var x = document.getElementById("navBar")
+    var y = document.getElementById('bottom')
     if(x.className === "topnav"){
         x.className += " responsive";
     }else{
         x.className = "topnav";
+    }
+    if(y.className === 'privacy'){
+        y.className += ' responsive';
+    }else{
+        y.className = 'privacy'
     }
 }
